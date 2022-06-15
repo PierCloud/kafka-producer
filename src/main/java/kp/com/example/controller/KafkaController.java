469a4cf23a1f6b396e94d3584b8c3c7a7a0b9a0b
pierdipilla22@gmail.com <8S&m?;,v8q,eF:d>
2022-06-16 00:23:30 +0200

    update and add swagger

--- src/main/java/kp/com/example/controller/KafkaController.java
+++ src/main/java/kp/com/example/controller/KafkaController.java
@@ -1,5 +1,6 @@
 package kp.com.example.controller;
 
+import io.swagger.annotations.*;
 import kp.com.example.producer.service.TopicProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/kafka")
+@Api(value = "HelloWorldSpringBootSwagger", tags = "Controller di prova ")
 public class KafkaController {
+    @ApiModelProperty(notes = "referenza al servizio producer")
     private final TopicProducerService topicProducer;
+
+    @ApiOperation(value = "inoltra payload dati su kafka", notes = "payload = string")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "messaggio inoltrato"),
+            @ApiResponse(code = 404, message = "none")})
+
     @GetMapping(value = "/send")
     public void send(){
         topicProducer.send("messaggio di prova del topic");
